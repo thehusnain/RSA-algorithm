0a154d838351cb6d402c0b2bfaa9ed57f7e9f094
Husnain <huxnain.cs@gmail.com>
2026-01-15 16:24:33 +0500

    Implement RSA encryption and decryption

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("RSA Algoithm")
-print("OK")
+M = 31
+e = 7
+d = 3
+n = 33
+
+# Encyption 
+encrypted = (M ** e) % n
+print("Encrypted:",encrypted)
+
+# Decryption
+decrypted = (encrypted ** d) % n
+print("Decrypted", decrypted)
